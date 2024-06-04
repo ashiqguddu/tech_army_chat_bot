f8f4f586b16a664c548bcf7247b8d8ca75ba46bf
ashiqguddu <ashikguddu007@gmail.com>
2024-06-04 09:55:57 +0530

    aashik

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,41 @@
-import React from 'react'
+import React from "react";
 import Login from "./Components/Login";
-import '..\\node_modules\\bootstrap\\dist\\css\\bootstrap.min.css'
-import { Route, Routes } from 'react-router-dom';
-import SignUp from './Components/SignUp';
-import Bot from './Components/bot';
+import "..\\node_modules\\bootstrap\\dist\\css\\bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./Components/SignUp";
+import Bot from "./Components/bot";
 
 const App = () => {
   return (
     <div>
       <nav className="navbar bg-dark m-0 nav-mar">
-  <div className="container-fluid nav-back">
-    <a className="navbar-brand" href="#">
-      <img src="../../src/assets\photo_2024-06-03_14-38-16.jpg" alt="Bootstrap" width="130" height="70"/>
-    </a>
-  </div>
-</nav>
-<div className=''>
-      <Login></Login>
-      
+        <div className="container-fluid nav-back">
+          <a className="navbar-brand" href="#">
+            <img
+              src="../../src/assets\photo_2024-06-03_14-38-16.jpg"
+              alt="Bootstrap"
+              width="130"
+              height="70"
+            />
+          </a>
+        </div>
+      </nav>
+      <div className="">
+        <Login> </Login>
       </div>
-      
 
-      
-
-{/* __________ */}
-
+      {/* __________ */}
 
       <div>
         <Routes>
           <Route path="/signup" element={<SignUp></SignUp>}></Route>
           <Route path="/" element={<Login></Login>}></Route>
-
         </Routes>
       </div>
     </div>
 
+    // _____
+  );
+};
 
-// _____
-
-
-  )
-}
-
-export default App
+export default App;
